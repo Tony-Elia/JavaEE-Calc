af26ab808282ee0218f4b8d7377a1ccc2cb2d553
Tony-Elia <antony.safaga@gmail.com>
2024-04-24 00:50:39 +0200

    rm services

--- src/main/java/APP/myApp.java
+++ src/main/java/APP/myApp.java
@@ -4,6 +4,6 @@ import javax.ws.rs.ApplicationPath;
 
 import javax.ws.rs.core.Application;
 @ApplicationPath("api")
-public class myAPPLICATION extends Application  {
+public class myApp extends Application  {
 
 }
